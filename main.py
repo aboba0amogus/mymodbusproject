ec0e98a15e23b24cbdeb483b60fdc718ec29383e
aboba0amogus <invalid0user0intelligence@gmail.com>
2026-02-25 22:14:01 +1000

    try to connect to device

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("helloworld")
+from pymodbus.client import ModbusSerialClient
+print("start main.py")
+
+client = ModbusSerialClient(
+    port="/dev/ttyUSB0",
+    baudrate=9600,
+    bytesize=8,
+    parity='E',
+    stopbits=2,
+    name="mymodbusproject")
+
+response = client.read_holding_registers(address=36864,count=1,device_id=10)
+print(response)
+
