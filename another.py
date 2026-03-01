0bd055053f236f3cb1af566647b23319ed1d1d70
aboba0amogus <invalid0user0intelligence@gmail.com>
2026-03-02 00:33:03 +1000

    another2

--- another.py
+++ another.py
@@ -6,31 +6,29 @@ baudrate = 9600
 parities = ['E','O','N']
 stopbits = [1,2]
 device_ids = [10,0]
+reg_addresses = [0,1,90,0x9000,0x9000-1,0x9000+1]
 
 for device_id in device_ids:
     for parity in parities:
         for stopbit in stopbits:
-            print(f"значения    baudrate:{baudrate}    parity:{parity}    stopbit:{stopbit}    id:{device_id}")
-
-            try:
-                client = ModbusSerialClient(
-                port="/dev/ttyUSB0",
-                baudrate=baudrate,
-                bytesize=8,
-                parity=parity,
-                stopbits=stopbit,
-                name="mymodbusproject")
-
-                reg_addresses = [0,1,90,0x9000,0x9000-1,0x9000+1]
-                for reg_address in reg_addresses:
+            for reg_address in reg_addresses:
+                print(f"значения    baudrate:{baudrate}    parity:{parity}    stopbit:{stopbit}    id:{device_id} reg_address:{reg_address}")
+                try:
+                    client = ModbusSerialClient(
+                    port="/dev/ttyUSB0",
+                    baudrate=baudrate,
+                    bytesize=8,
+                    parity=parity,
+                    stopbits=stopbit,
+                    name="mymodbusproject")
                     response  = client.read_holding_registers(address=0x9000,count=1,device_id=device_id)
                     print(response)
                     print("YES!")
-            except Exception as e:
-                print("NOT")
-                print(e)
-            finally:
-                client.close()
+                except Exception as e:
+                    print("NOT")
+                    print(e)
+                finally:
+                    client.close()
 print("команда завершена")
 
 
